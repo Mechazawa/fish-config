abf64a08aca045a16e38a9cda86a883628d787d4
Bas <Mechazawa@users.noreply.github.com>
2023-09-05 15:10:16 +0200

    Create project_bin.fish

---
+++ conf.d/project_bin.fish
@@ -0,0 +1,37 @@
+# Used for adding vendor/bin and node_modules/.bin to my path when in a project (sub)directory
+
+function find_nearest
+    set -l dir (pwd)
+    set -l target $argv[1]
+    while test "$dir" != "/"
+        set -l candidate "$dir/$target"
+        if test -d "$candidate"
+            echo "$candidate"
+            return 0
+        end
+        set dir (dirname "$dir")
+    end
+    return 1
+end
+
+function update_project_bin_path --on-event fish_prompt
+    # Initialize default_path if not set
+    if not set -q default_path
+        set -g default_path $PATH
+    end
+
+    set -l nearest_vendor_bin (find_nearest "vendor/bin")
+    set -l nearest_node_bin (find_nearest "node_modules/.bin")
+    
+    set -l temp_path $default_path
+
+    if test -n "$nearest_vendor_bin"
+        set temp_path $nearest_vendor_bin $temp_path
+    end
+
+    if test -n "$nearest_node_bin"
+        set temp_path $nearest_node_bin $temp_path
+    end
+
+    set -gx PATH $temp_path
+end
